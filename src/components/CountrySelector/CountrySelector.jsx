c2b5f0cbdf2dbffd1abf45fb2c86a87ec8567aac
Ihsan Muhammad Aulia <ihsanauliaa@gmail.com>
2020-12-20 22:37:17 +0700

    Add mock response for country list

--- src/components/CountrySelector/CountrySelector.jsx
+++ src/components/CountrySelector/CountrySelector.jsx
@@ -2,17 +2,36 @@ import React, { Component } from "react";
 import styles from './CountrySelector.module.css'
 import Select from "react-select";
 
-const countries = [
-    { label: 'Indonesia', value: 'Indonesia' },
-    { label: 'United States', value: 'United States' },
-    { label: 'United Kingdom', value: 'United Kingdom' },
-    { label: 'France', value: 'France' },
-    { label: 'Germany', value: 'Germany' },
-    { label: 'Canada', value: 'Canada' },
-];
-
 class CountrySelector extends Component {
+    state = {
+        countryList: []
+    }
+
+    componentDidMount() {
+        this.fetchCountry();
+    }
+
+    fetchCountry = () => {
+        const URL = "https://run.mocky.io/";
+        fetch(URL + "v3/3441cc29-0c37-41c7-873b-899c7e1535a6", {
+            method: "GET",
+        }).then(response => {
+            return response.json();
+        }).then((data) => {
+            const fetchedData = [];
+            for (let key in data.result) {
+                fetchedData.push({
+                    ...data.result[key]
+                });
+            }
+            this.setState({
+                countryList: fetchedData
+            });
+        })
+    }
+
     render() {
+        console.log(this.state.countryList);
         return (
             <React.Fragment>
                 <div style={{ display: "inline-block"}}>
@@ -20,7 +39,7 @@ class CountrySelector extends Component {
                 </div>
                 <div style={{ display: "inline-block", minWidth: "40%", marginLeft: "10px"}}>
                     <Select
-                        options={countries}
+                        options={this.state.countryList.map(opt => ({ label: opt.name, value: opt.code }))}
                         onChange={opt => console.log(opt.value)}
                     />
                 </div>
